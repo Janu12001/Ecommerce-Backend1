388e39c7b868a5d27015eeddeedbb26397d55596
Janu <99030758+Janu12001@users.noreply.github.com>
2023-04-28 18:17:35 +0000

    added morgan and cors

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 import express from 'express'
+import cookieParser from'cookie-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(cookieParser())
+
+//morgan logger- prints info about the api request or response in console
+app.use(morgan('tiny'))
+
 
 export default app
