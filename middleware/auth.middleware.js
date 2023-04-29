0eb15592c222482d111511a626e98cb5de6c8eb8
Janu <99030758+Janu12001@users.noreply.github.com>
2023-04-29 18:26:25 +0000

    added middleware for loggedin

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+import User from "../models/user.schema.js"
+import JWT from 'jsonwebtoken'
+import asyncHandler from '../services/asyncHandler'
+import CustomError from '../utils/CustomError'
+import config from"../config/index.js"
+ 
+
+export const isloggedin = asyncHandler(async(req,res,next)=>{
+    let token;
+
+    if(req.cookies.token|| (req,headers.authorization && req.headers.authorization.startWith("Bearer"))
+     ) {
+        token = req.cookie.token || req.headers.authorization.split(" ")[1]
+    }
+
+    if(!token){
+        throw new CustomError("not authorized to acess this route", 401)
+    }
+
+    try{
+        const decodedJwtpayload = JWT.verify(token, config.JWT_SECRET)
+        //_id , find user based onid  , set this in req.user
+      req.user = await  User.findById(decodedJwtpayload._id, "name email role")
+      next()
+    }catch(err){
+        throw new CustomError("not authorized to access this route", 401)
+    }
+})
